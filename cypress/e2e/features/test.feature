a126dd68b76d5b5c32d46191853056befa15dd88
ChristelNunez <christelnunez@bizkittechnologies.com>
2022-10-06 18:05:57 +0800

    Added test cases for change password

--- cypress/e2e/features/test.feature
+++ cypress/e2e/features/test.feature
@@ -6,12 +6,36 @@ Given I am on on the Login page
 Scenario: Login
 When When I login an invalid credentials
 | email                                  | password         |
-| sample@bizkittechnologies.com | 12345 |
+| sample@bizkittechnologies.com          | 12345            |
 
 Then I should see an error
 
 When When I login a valid credentials
 | email                                  | password         |
-| phoelenealcober@bizkittechnologies.com | phoelene_alcober |
+| fayecatalvas@bizkittechnologies.com    | faye_catalvas    |
 
 Then I should redirect to home page
+
+Scenario: Change Password
+When I successfully login
+| email                                  | password         |
+| fayecatalvas@bizkittechnologies.com    | faye_catalvas    |
+Then I should be redirected to homepage
+
+When I go to My Settings
+Then I should be redirected to settings page
+
+When I enter a sequenced password
+| password         |
+| abcdefg          |
+Then I should see an error message
+
+When I enter a common password
+| password              |
+| testpassword          |
+Then I should see an error msg
+
+When I enter a valid password
+| password              |
+| testpass123!          |
+Then New password should be saved
--- cypress/support/step_definitions/test.js
+++ cypress/support/step_definitions/test.js
@@ -18,7 +18,7 @@ When('When I login an invalid credentials', (datable) => {
 Then('I should see an error', () => {
     cy.wait(2000)
     cy.get('.btn-login').should('have.html', 'Invalid Login. Try again.')
-})
+});
 
 When('When I login a valid credentials', (datable) => {
     datable.hashes().forEach((el) => {
@@ -34,4 +34,71 @@ Then('I should redirect to home page', () => {
     cy.wait(5000)
     cy.url().should('include', '/app')
     cy.get('.page-title').contains('Home')
-})
+});
+
+When('I successfully login', (datable) => {
+    datable.hashes().forEach((el) => {
+        cy.get('#login_email').clear();
+        cy.get('#login_email').type(el.email);
+        cy.get('#login_password').clear();
+        cy.get('#login_password').type(el.password);
+        cy.get('.btn-login').click();
+    })
+});
+
+Then('I should be redirected to homepage', () => {
+    cy.wait(5000)
+    cy.url().should('include', '/app')
+    cy.get('.page-title').contains('Home')
+});
+
+When('I go to My Settings', () => {
+    cy.get('.avatar-frame').click();
+    cy.get('[onclick="return frappe.ui.toolbar.route_to_user()"]').click();    
+});
+
+Then('I should be redirected to settings page', () => {
+    cy.url().should('include', '/app/user')
+    cy.wait(5000)
+});
+
+When('I enter a sequenced password', (data) => {
+    cy.get('.section-head').contains('Change Password').click();
+    data.hashes().forEach((pw) => {
+        cy.get('input[type=password]').type(pw.password);
+        cy.get('[data-label="Save"]').click();
+    })
+});
+
+Then('I should see an error message', () => {
+    cy.get('.msgprint').should('include.text','Avoid sequences')
+    cy.get('.btn-modal-close').click();
+    cy.wait(5000)
+});
+
+When('I enter a common password', (data) => {
+    data.hashes().forEach((pw) => {
+        cy.get('input[type=password]').clear();
+        cy.get('input[type=password]').type(pw.password);
+        cy.get('[data-label="Save"]').click();
+    })
+});
+
+Then('I should see an error msg', () => {
+    cy.get('.msgprint').should('include.text','commonly used password')
+    cy.get('.btn-modal-close').click();
+});
+
+When('I enter a valid password', (data) => {
+    data.hashes().forEach((pw) => {
+        cy.get('input[type=password]').clear();
+        cy.get('input[type=password]').type(pw.password);
+        cy.get('[data-label="Save"]').click();
+    })
+});
+
+Then('New password should be saved', () => {
+    cy.get('.alert-message').should('include.text','Saved')
+});
+
+
